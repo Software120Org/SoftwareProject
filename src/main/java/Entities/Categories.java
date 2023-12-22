206253d3af20692f670e86b9b229928c6357ef56
wafaa manal <145779576+Wafaa-Hamed@users.noreply.github.com>
2023-12-22 22:47:59 +0200

    Add files via upload

---
+++ src/main/java/Entities/Categories.java
@@ -0,0 +1,60 @@
+package Entities;
+
+public class Categories {
+    private Integer id;
+    private String name;
+    private Categories existCategories;
+
+    public Categories(){
+
+    }
+
+    public Categories(Integer id,String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public Categories(String name){
+        this.id=Database.getCategoryId();
+        this.name=name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Categories getCategories() {
+        return existCategories;
+    }
+
+    @Override
+    public String toString() {
+        return  id +
+                "," + name  +"\r\n";
+    }
+    public boolean isExitCategories(){
+        boolean flag=false;
+        for(Categories category:Database.getCategories()){
+            if(category.getId()==this.id){
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+
+
+
+}
