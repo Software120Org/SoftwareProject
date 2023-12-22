474eb77960d6ec49884add22ea0901552f1b6d89
anwar-aqraa <145788070+anwar-aqraa@users.noreply.github.com>
2023-12-22 15:32:28 +0200

    Add files via upload

---
+++ src/test/java/Tests/UpdateCategory.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import org.Classes.AdminDashboard;
+import org.entities.Categories;
+import org.entities.Database;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class UpdateCategory {
+    AdminDashboard admin;
+    Categories categories;
+
+    String value;
+
+    @Given("that the admin choose to update category")
+    public void that_the_admin_choose_to_update_category() {
+        admin=new AdminDashboard();
+        categories=Database.getCategoriesById(4);
+
+    }
+    @When("i select to update category name = {string}")
+    public void i_select_to_update_category_name(String name) {
+
+        admin.updateCategory(name,categories);
+        value=name;
+    }
+    @Then("category name will updated successfully")
+    public void category_name_will_updated_successfully() {
+        assertEquals(categories.getName(), value);
+        System.out.println("Category updated successfully :)");
+    }
+
+
+
+
+}
