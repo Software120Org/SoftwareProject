eca1502550172a908105113c822d14c43524f28d
wafaa manal <145779576+Wafaa-Hamed@users.noreply.github.com>
2023-12-23 16:42:34 +0200

    Add files via upload

---
+++ src/main/java/org/classes/addInstall.java
@@ -0,0 +1,25 @@
+package org.classes;
+import org.entities.Database;
+import org.entities.addinstalldate;
+import java.util.Scanner;
+import java.util.logging.Logger;
+public class addInstall {
+    public final static java.util.logging.Logger logger = Logger.getLogger(addInstall.class.getName());
+    public static addinstalldate recordInstall() {
+        Scanner in=new Scanner(System.in);
+        addinstalldate addInstallDate=new addinstalldate();
+        logger.info("Enter installation Date");
+        String date =in.nextLine();
+        addInstallDate.setDate(date);
+        logger.info("Enter installation Time ");
+        String time =in.nextLine();
+        addInstallDate.setTime(time);
+        addInstallDate.setId(Database.getInstallId());
+        addInstallDate.setStatus("Available");
+        addNewDate(addInstallDate);
+        return addInstallDate;
+    }
+    public static void addNewDate(addinstalldate addInstallDate){
+        Database.storeObject("dates",addInstallDate);
+    }
+}
