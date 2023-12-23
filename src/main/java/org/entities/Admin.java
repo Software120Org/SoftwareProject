5ce60c06bee33cac0083fa59ec0e3f013b1271d6
anwar-aqraa <145788070+anwar-aqraa@users.noreply.github.com>
2023-12-23 17:30:33 +0200

    Add files via upload

---
+++ src/main/java/org/entities/Admin.java
@@ -0,0 +1,19 @@
+package org.entities;
+
+public class Admin {
+    private String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public Admin() {
+          // empty constructor
+    }
+
+
+
+}
